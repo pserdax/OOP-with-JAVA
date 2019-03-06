76fa9b75382ede4c00e5cb682f9e61a80278794f
Serdar Parahatgeldiyev <39422653+serdarpp@users.noreply.github.com>
2019-03-06 19:50:34 +0200

    Add files via upload

---
+++ week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -0,0 +1,23 @@
+public class GuessingNumberGame{
+public static void main(String[] args){
+
+int number  = 1;
+
+    System.out.println("The number introduced is: "+ number );
+    addThree(number);
+    System.out.println("The number introduced is: "+ number );
+    
+    
+
+
+}
+public static void addThree(int number){
+
+    System.out.println("The number introduced is:"+ number);
+number =number +3;
+    System.out.println(" The number introduced is:"+ number );
+
+}
+
+
+}
---
+++ week3-043.SumOfNumbers/src/SumOfNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfNumbers{
+
+    public static void main(String[] args){
+   
+      int answer = sum(4,3,6,1);
+      
+        System.out.println("sum:"+answer);
+        
+    
+    
+    
+    }
+    
+    public static int sum(int number1, int number2, int number3, int number4){
+    
+    
+    return (number1+number2+number3+number4);
+    
+    }
+}
