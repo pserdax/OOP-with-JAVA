67bcd74775e434d9ff825149208e956ca300812b
Serdar Parahatgeldiyev <39422653+serdarpp@users.noreply.github.com>
2019-03-06 19:44:37 +0200

    Add files via upload

---
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,16 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        int number = 1;
+        while (number<=100) {
+            
+            System.out.println(number);
+            number++;
+            
+                   
+           
+            
+        }
+    }
+}
---
+++ week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class UpToCertainNumber {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+       int number = 1;
+        System.out.println("Up to what number? ");
+       int limit  = Integer.parseInt(reader.nextLine());
+       
+       while(number <= limit){
+           System.out.println(number);
+           number++;
+           
+       }
+       
+       
+    }
+}
